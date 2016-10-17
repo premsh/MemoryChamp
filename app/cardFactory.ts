64deb39f8c213f221f935a77f49669593f3f5df2
preohm <prem.shrestha@fcsamerica.com>
2016-10-16 22:38:32 -0500

    Cards are now randomly generated!

--- app/cardFactory.ts
+++ app/cardFactory.ts
@@ -10,17 +10,31 @@ export class CardFactory {
 
     createCards(numberOfRows: number = 3, numberOfColumns: number = 3): any[] {
         let originalNumberOfColumns: number = numberOfColumns;
+        let cardCounter = 0;
+        let cardContents = this.createLinearCards(numberOfRows * numberOfColumns);
         for (var i = 0; i < numberOfRows; i++) {
             this.cards[i] = [];
             while (numberOfColumns > 0) {
-                let cardContent = this.getRandomCardContent();
-                let card = new CardModel(cardContent);
+                let card = new CardModel(cardContents[cardCounter++]);
                 this.cards[i].push(card);
                 numberOfColumns--;
             }
             numberOfColumns = originalNumberOfColumns;
         }
         return this.cards;
+    }
+
+    createLinearCards(numberOfCards: number): CardContent[] {
+        let cardContents = new Array<CardContent>();
+
+        let randomizer = new Randomizer();
+        let randomIndexedCards = randomizer.getSetOfRandomIndex(numberOfCards);
+        var cardData = new CardData();
+        cardData.fillCardData();
+        for (let i = 0; i < randomIndexedCards.length; i++) {
+            cardContents.push(cardData.cardContents[randomIndexedCards[i]]);
+        }
+        return cardContents;
     }
 
     getRandomCardContent(): CardContent {
