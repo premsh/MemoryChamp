fdcad06efa05cf271f47c2e381ccda4f97bdf1e0
preohm <prem.shrestha@fcsamerica.com>
2016-10-18 22:54:14 -0500

    initial phase of game complete! Milestone 1 reached!! wooo hoo!!

--- app/cards.component.js
+++ app/cards.component.js
@@ -11,10 +11,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var Cards = (function () {
     function Cards() {
+        this.setsToMatch = 6;
+        this.message = '';
         this.firstActiveCardModel = undefined;
         this.secondActiveCardModel = undefined;
     }
     Cards.prototype.cardClicked = function (cardModel) {
+        var gameMessage = new GameMessage();
         console.log('got clicked!', cardModel.cardContent.cardValue);
         if (this.firstActiveCardModel === undefined) {
             this.firstActiveCardModel = cardModel;
@@ -23,16 +26,28 @@ var Cards = (function () {
             if (this.secondActiveCardModel === undefined) {
                 this.secondActiveCardModel = cardModel;
                 if (this.firstActiveCardModel.cardContent.cardValue === this.secondActiveCardModel.cardContent.cardValue) {
-                    alert('first match!' + this.firstActiveCardModel.cardContent.cardValue + this.secondActiveCardModel.cardContent.cardValue);
-                }
-                else {
-                    this.firstActiveCardModel.isFlip = false;
-                    this.secondActiveCardModel.isFlip = false;
                     this.firstActiveCardModel = undefined;
                     this.secondActiveCardModel = undefined;
+                    this.message = gameMessage.goodJobMessage;
+                    this.setsToMatch--;
+                    if (this.setsToMatch === 0) {
+                        this.message = gameMessage.allMatchedMessage;
+                    }
+                }
+                else {
+                    this.flipCardsAfterGivenSeconds(this.firstActiveCardModel, this.secondActiveCardModel, 2000);
+                    this.firstActiveCardModel = undefined;
+                    this.secondActiveCardModel = undefined;
+                    this.message = gameMessage.keepTryingMessage;
                 }
             }
         }
+    };
+    Cards.prototype.flipCardsAfterGivenSeconds = function (firstCard, secondCard, milliseconds) {
+        setTimeout(function () {
+            firstCard.isFlip = false;
+            secondCard.isFlip = false;
+        }, milliseconds);
     };
     __decorate([
         core_1.Input(), 
@@ -41,11 +56,21 @@ var Cards = (function () {
     Cards = __decorate([
         core_1.Component({
             selector: 'cards',
-            template: "\t\t\t\t\t  \n\t\t\t\t<table>\n\t\t\t\t\t<tr *ngFor=\"let cardmodelItem of cardmodels; trackBy : index\">\n\t\t\t\t\t\t<td *ngFor=\"let cardmodel of cardmodelItem; trackBy : index\">\n\t\t\t\t\t\t\t<card [cardmodel] = \"cardmodel\" (cardClicked) = \"cardClicked($event)\"> </card>\n\t\t\t\t\t\t</td>\n\t\t\t\t\t</tr>\n\t\t\t\t</table>\n  \t\t\t\t"
+            template: "\t\t\t\n\t\t\t\t<h1> Sets to match {{ setsToMatch }} </h1>\n\t\t\t\t<h2> {{ message }} </h2>\t\t  \n\t\t\t\t<table>\n\t\t\t\t\t<tr *ngFor=\"let cardmodelItem of cardmodels; trackBy : index\">\n\t\t\t\t\t\t<td *ngFor=\"let cardmodel of cardmodelItem; trackBy : index\">\n\t\t\t\t\t\t\t<card [cardmodel] = \"cardmodel\" (cardClicked) = \"cardClicked($event)\"> </card>\n\t\t\t\t\t\t</td>\n\t\t\t\t\t</tr>\n\t\t\t\t</table>\n  \t\t\t\t"
         }), 
         __metadata('design:paramtypes', [])
     ], Cards);
     return Cards;
 }());
 exports.Cards = Cards;
+var GameMessage = (function () {
+    function GameMessage() {
+        this.welcomeMessage = 'Welcome to the Memory Game!';
+        this.goodJobMessage = 'Good Job! Keep matching the pairs!';
+        this.keepTryingMessage = 'You almost had that! Keep trying!';
+        this.allMatchedMessage = 'Woo hoo!! Great! You have matched all the pairs! You are off to next level now!';
+    }
+    return GameMessage;
+}());
+exports.GameMessage = GameMessage;
 //# sourceMappingURL=cards.component.js.map
--- app/cards.component.ts
+++ app/cards.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
-import {CardModel} from './card.component';
+import { CardModel } from './card.component';
 @Component({
 	selector: 'cards',
-	template: `					  
+	template: `			
+				<h1> Sets to match {{ setsToMatch }} </h1>
+				<h2> {{ message }} </h2>		  
 				<table>
 					<tr *ngFor="let cardmodelItem of cardmodels; trackBy : index">
 						<td *ngFor="let cardmodel of cardmodelItem; trackBy : index">
@@ -15,38 +17,58 @@ import {CardModel} from './card.component';
 export class Cards {
 	@Input() cardmodels;
 
-	firstActiveCardModel : CardModel;
-	secondActiveCardModel : CardModel;
+	setsToMatch: number = 6;
+	message: string = '';
+
+	firstActiveCardModel: CardModel;
+	secondActiveCardModel: CardModel;
 
 	constructor() {
 		this.firstActiveCardModel = undefined;
 		this.secondActiveCardModel = undefined;
 	}
 
-	cardClicked(cardModel : CardModel) {
-		console.log('got clicked!' , cardModel.cardContent.cardValue);
-		if(this.firstActiveCardModel === undefined)
-		{
+	cardClicked(cardModel: CardModel) {
+		let gameMessage = new GameMessage();
+		console.log('got clicked!', cardModel.cardContent.cardValue);
+		if (this.firstActiveCardModel === undefined) {
 			this.firstActiveCardModel = cardModel;
 		}
 		else {
-			if(this.secondActiveCardModel === undefined)
-			{
+			if (this.secondActiveCardModel === undefined) {
 				this.secondActiveCardModel = cardModel;
-				if(this.firstActiveCardModel.cardContent.cardValue === this.secondActiveCardModel.cardContent.cardValue)
-				{
-					alert('first match!' + this.firstActiveCardModel.cardContent.cardValue + this.secondActiveCardModel.cardContent.cardValue);
-				}
-				else {
-					this.firstActiveCardModel.isFlip = false;
-					this.secondActiveCardModel.isFlip = false;
+				if (this.firstActiveCardModel.cardContent.cardValue === this.secondActiveCardModel.cardContent.cardValue) {
 					this.firstActiveCardModel = undefined;
 					this.secondActiveCardModel = undefined;
+					this.message = gameMessage.goodJobMessage;
+					this.setsToMatch--;
+					if(this.setsToMatch === 0)
+					{
+						this.message = gameMessage.allMatchedMessage;
+					}
+				}
+				else {
+					this.flipCardsAfterGivenSeconds(this.firstActiveCardModel, this.secondActiveCardModel, 2000);
+					this.firstActiveCardModel = undefined;
+					this.secondActiveCardModel = undefined;
+					this.message = gameMessage.keepTryingMessage;
 				}
 			}
 		}
+	}
 
+	flipCardsAfterGivenSeconds(firstCard: CardModel, secondCard: CardModel, milliseconds: number): void {
+		setTimeout(function () {
+			firstCard.isFlip = false;
+			secondCard.isFlip = false;
+		}, milliseconds);
 	}
 }
 
 
+export class GameMessage {
+	welcomeMessage : string = 'Welcome to the Memory Game!';
+	goodJobMessage: string = 'Good Job! Keep matching the pairs!';
+	keepTryingMessage: string = 'You almost had that! Keep trying!';
+	allMatchedMessage: string = 'Woo hoo!! Great! You have matched all the pairs! You are off to next level now!';
+}
